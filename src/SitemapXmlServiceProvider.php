a96adcd04106bdbfd2f238f021dfc024abcc0963
Andrey <0test@mail.ru>
2022-09-14 20:31:56 +0300

    Create SitemapXmlServiceProvider.php

---
+++ src/SitemapXmlServiceProvider.php
@@ -0,0 +1,23 @@
+<?php 
+namespace EvolutionCMS\SitemapXml;
+use EvolutionCMS\ServiceProvider;
+use EvolutionCMS\SitemapXml\Controllers\SitemapXML;
+
+class SitemapXmlServiceProvider extends ServiceProvider
+{
+    protected $namespace = 'sitemapxml';
+    
+    public function register()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
+        $this->app->alias(SitemapXML::class, 'sitemapxml');
+    }
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../views', $this->namespace);
+
+        $this->publishes([
+            __DIR__ . '/../publishable/configs' => EVO_CORE_PATH .'custom/packages/sitemapxml',
+        ]);
+    }
+}
